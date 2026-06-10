81f2ed31f1cc4679dfc9fb3831f1bd3aa40347fb
Oleksandr <dllprivetllb@gmail.com>
2026-06-10 20:58:35 +0300

    Updt game, added new shapes

--- lib/cyber_runner_game.dart
+++ lib/cyber_runner_game.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class CyberRunnerGame extends StatefulWidget {
@@ -23,12 +24,16 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
   double _playerY = 0.0;
   double _velocity = 0.0;
   final double _gravity = 0.009;
-  final double _jumpForce = -0.13;
+  final double _jumpForce = -0.10;
   bool _isJumping = false;
   double _playerRotation = 0.0;
-  double _obstacleX = 1.3;
+
+  // Множинні перешкоди
+  final List<Map<String, dynamic>> _obstacles = [];
   final double _obstacleSpeed = 0.035;
-  int _obstacleType = 0;
+  final double _minObstacleDistance =
+      1.2; // Мінімальна відстань між перешкодами (збільшено)
+  final Random _random = Random();
 
   @override
   void dispose() {
@@ -44,14 +49,18 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
       _playerY = 0.0;
       _velocity = 0.0;
       _playerRotation = 0.0;
-      _obstacleX = 1.3;
-      _obstacleType = 0;
       _isJumping = false;
+      _obstacles.clear();
+
+      // Створюємо початкові перешкоди (тільки одна на старті)
+      _spawnObstacle(1.5);
     });
 
     _gameTimer = Timer.periodic(const Duration(milliseconds: 16), (timer) {
       setState(() {
         _gameScore++;
+
+        // Фізика гравця
         if (_isJumping || _playerY < 0) {
           _velocity += _gravity;
           _playerY += _velocity;
@@ -63,23 +72,72 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
           _isJumping = false;
           _playerRotation = 0.0;
         }
-        _obstacleX -= (_obstacleSpeed + (_gameScore / 30000));
-        if (_obstacleX < -1.2) {
-          _obstacleX = 1.3;
-          _obstacleType = DateTime.now().microsecond % 3;
+
+        // Оновлення перешкод
+        double speedBoost = _gameScore / 30000;
+        for (var obstacle in _obstacles) {
+          obstacle['x'] -= (_obstacleSpeed + speedBoost);
         }
-        if (_obstacleX > -0.72 && _obstacleX < -0.48) {
-          if (_obstacleType == 0 && _playerY > -0.12) _endGame();
-          if (_obstacleType == 1 &&
-              _obstacleX > -0.76 &&
-              _obstacleX < -0.44 &&
-              _playerY > -0.12) {
-            _endGame();
-          }
-          if (_obstacleType == 2 && _playerY > -0.28) _endGame();
+
+        // Видалення перешкод, що вийшли за екран
+        _obstacles.removeWhere((obstacle) => obstacle['x'] < -1.2);
+
+        // Створення нових перешкод (рідше)
+        if (_obstacles.isEmpty || _obstacles.last['x'] < 0.5) {
+          double newX = _obstacles.isEmpty
+              ? 1.5
+              : _obstacles.last['x'] +
+                    _minObstacleDistance +
+                    (_random.nextDouble() * 0.6);
+          _spawnObstacle(newX);
         }
+
+        // Перевірка колізій
+        _checkCollisions();
       });
     });
+  }
+
+  void _spawnObstacle(double x) {
+    int type = _random.nextInt(5); // 5 типів перешкод
+    _obstacles.add({'x': x, 'type': type});
+  }
+
+  void _checkCollisions() {
+    for (var obstacle in _obstacles) {
+      double obstacleX = obstacle['x'];
+      int obstacleType = obstacle['type'];
+
+      // Перевірка, чи гравець перетинається з перешкодою
+      if (obstacleX > -0.72 && obstacleX < -0.48) {
+        bool collision = false;
+
+        switch (obstacleType) {
+          case 0: // Трикутник
+            if (_playerY > -0.12) collision = true;
+            break;
+          case 1: // Подвійний трикутник
+            if (obstacleX > -0.76 && obstacleX < -0.44 && _playerY > -0.12) {
+              collision = true;
+            }
+            break;
+          case 2: // Висока коробка
+            if (_playerY > -0.28) collision = true;
+            break;
+          case 3: // Круг
+            if (_playerY > -0.15) collision = true;
+            break;
+          case 4: // Діамант
+            if (_playerY > -0.18) collision = true;
+            break;
+        }
+
+        if (collision) {
+          _endGame();
+          break;
+        }
+      }
+    }
   }
 
   void _onGameTap() {
@@ -143,7 +201,7 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
                   children: [
                     // Лінія підлоги
                     Positioned(
-                      bottom: 40,
+                      bottom: 80,
                       left: 0,
                       right: 0,
                       child: Container(
@@ -159,7 +217,7 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
                     // Гравець (Куб)
                     AnimatedAlign(
                       duration: const Duration(milliseconds: 0),
-                      alignment: Alignment(-0.6, 0.45 + _playerY),
+                      alignment: Alignment(-0.6, 0.25 + _playerY),
                       child: Transform.rotate(
                         angle: _playerRotation,
                         child: Container(
@@ -189,12 +247,14 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
                         ),
                       ),
                     ),
-                    // Перешкода
-                    AnimatedAlign(
-                      duration: const Duration(milliseconds: 0),
-                      alignment: Alignment(_obstacleX, 0.45),
-                      child: _buildObstacleWidget(),
-                    ),
+                    // Всі перешкоди
+                    ..._obstacles.map((obstacle) {
+                      return AnimatedAlign(
+                        duration: const Duration(milliseconds: 0),
+                        alignment: Alignment(obstacle['x'], 0.25),
+                        child: _buildObstacleWidget(obstacle['type']),
+                      );
+                    }).toList(),
                     // Екран старту
                     if (!_gameStarted)
                       Center(
@@ -207,23 +267,26 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
                             color: Colors.black.withOpacity(0.8),
                             borderRadius: BorderRadius.circular(10),
                           ),
-                          child: const Column(
+                          child: Column(
                             mainAxisSize: MainAxisSize.min,
                             children: [
                               Text(
                                 "⚡ CYBER RUNNER ⚡",
                                 style: TextStyle(
-                                  color: Colors.white,
+                                  color: widget.neonPink,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 12,
+                                  fontSize: 16,
+                                  letterSpacing: 2,
+                                  decoration: TextDecoration.none,
                                 ),
                               ),
-                              SizedBox(height: 4),
-                              Text(
-                                "TAP TO JUMP & UPDATE GRAPH",
+                              const SizedBox(height: 8),
+                              const Text(
+                                "TAP TO JUMP",
                                 style: TextStyle(
-                                  color: Colors.white38,
-                                  fontSize: 9,
+                                  color: Colors.white70,
+                                  fontSize: 12,
+                                  decoration: TextDecoration.none,
                                 ),
                               ),
                             ],
@@ -239,7 +302,8 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
                         style: TextStyle(
                           color: widget.neonPink,
                           fontWeight: FontWeight.bold,
-                          fontSize: 11,
+                          fontSize: 14,
+                          decoration: TextDecoration.none,
                         ),
                       ),
                     ),
@@ -253,52 +317,147 @@ class _CyberRunnerGameState extends State<CyberRunnerGame> {
     );
   }
 
-  Widget _buildObstacleWidget() {
-    if (_obstacleType == 0) {
-      return ClipPath(
-        clipper: CyberSpikeClipper(),
-        child: Container(width: 20, height: 26, color: widget.neonPink),
-      );
-    } else if (_obstacleType == 1) {
-      return Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ClipPath(
-            clipper: CyberSpikeClipper(),
-            child: Container(
-              width: 16,
-              height: 26,
-              color: const Color(0xFFFF0055),
+  Widget _buildObstacleWidget(int type) {
+    switch (type) {
+      case 0: // Трикутник (рожевий)
+        return ClipPath(
+          clipper: CyberSpikeClipper(),
+          child: Container(width: 20, height: 26, color: widget.neonPink),
+        );
+
+      case 1: // Подвійний трикутник (червоний)
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipPath(
+              clipper: CyberSpikeClipper(),
+              child: Container(
+                width: 16,
+                height: 26,
+                color: const Color(0xFFFF0055),
+              ),
             ),
-          ),
-          ClipPath(
-            clipper: CyberSpikeClipper(),
-            child: Container(
-              width: 16,
-              height: 26,
-              color: const Color(0xFFFF0055),
+            ClipPath(
+              clipper: CyberSpikeClipper(),
+              child: Container(
+                width: 16,
+                height: 26,
+                color: const Color(0xFFFF0055),
+              ),
             ),
-          ),
-        ],
-      );
-    } else {
-      return Container(
-        width: 18,
-        height: 42,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.white, width: 2),
-          boxShadow: [
-            BoxShadow(color: widget.neonPink.withOpacity(0.5), blurRadius: 6),
           ],
-        ),
-        child: Center(
-          child: Container(width: 4, height: 30, color: widget.neonPink),
-        ),
-      );
+        );
+
+      case 2: // Висока коробка (рожева)
+        return Container(
+          width: 22,
+          height: 42,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(color: widget.neonPink, width: 2),
+            borderRadius: BorderRadius.circular(4),
+            boxShadow: [
+              BoxShadow(
+                color: widget.neonPink.withOpacity(0.6),
+                blurRadius: 10,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Container(
+              width: 6,
+              height: 34,
+              decoration: BoxDecoration(
+                color: widget.neonPink,
+                boxShadow: [
+                  BoxShadow(
+                    color: widget.neonPink.withOpacity(0.8),
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
+      case 3: // Круг (блакитний)
+        return Container(
+          width: 28,
+          height: 28,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            shape: BoxShape.circle,
+            border: Border.all(color: const Color(0xFF00D9FF), width: 2),
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xFF00D9FF).withOpacity(0.6),
+                blurRadius: 10,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Container(
+              width: 12,
+              height: 12,
+              decoration: BoxDecoration(
+                color: const Color(0xFF00D9FF),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF00D9FF).withOpacity(0.8),
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
+      case 4: // Діамант (фіолетовий)
+        return ClipPath(
+          clipper: DiamondClipper(),
+          child: Container(
+            width: 26,
+            height: 32,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              border: Border.all(color: const Color(0xFFAA00FF), width: 2),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xFFAA00FF).withOpacity(0.6),
+                  blurRadius: 10,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: Center(
+              child: Container(
+                width: 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFAA00FF),
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFFAA00FF).withOpacity(0.8),
+                      blurRadius: 8,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+
+      default:
+        return Container();
     }
   }
 }
 
+// Трикутник
 class CyberSpikeClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
@@ -306,6 +465,23 @@ class CyberSpikeClipper extends CustomClipper<Path> {
     path.moveTo(size.width / 2, 0);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+// Діамант
+class DiamondClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(size.width, 0); // Верх
+    path.lineTo(size.width, size.height / 2); // Право
+    path.lineTo(size.width, size.height); // Низ
+    path.lineTo(0, size.height / 2); // Ліво
     path.close();
     return path;
   }
