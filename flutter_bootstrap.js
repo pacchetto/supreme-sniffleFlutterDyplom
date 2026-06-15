3adccd3705a43f50f6ed952f8a39cca183459521
pacchetto <pacchetto@users.noreply.github.com>
2026-06-15 20:18:04 +0000

    deploy: 8488082d20b481f51e0909f12bd88d58a2e2f16d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3701643514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "855094127" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
